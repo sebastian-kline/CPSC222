ae43358c794be50c1e274273cf152983bcd20680
sebastian <sebastian@linux.myguest.virtualbox.org>
2024-04-07 22:36:56 -0400

    PHP Chs 5-6 Letter Grade Function

---
+++ letterGrade.php
@@ -0,0 +1,18 @@
+<?php
+
+function calculateLetterGrade($grade) {
+	if ($grade >= 90) {
+		return 'A';
+	} elseif ($grade >= 80) {
+		return 'B';
+	} elseif ($grade >= 70) {
+		return 'C';
+	} elseif ($grade >= 60) {
+		return 'D';
+	} else {
+		return 'F';
+	}
+
+}
+
+?>
